38261b003df9b5f4acc7a1c915154ab79fff8d96
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-03-06 10:58:50 -0500

    Add _messagingInitialized global and messagingInitialized() helper to dg_firebase namespace.

--- src/_dg_firebase.js
+++ src/_dg_firebase.js
@@ -1,3 +1,8 @@
 dg.createModule('dg_firebase');
 
 dg_firebase._initialized = false;
+
+dg_firebase._messagingInitialized = false;
+dg_firebase.messagingInitialized = function() {
+  return dg_firebase._messagingInitialized;
+};
